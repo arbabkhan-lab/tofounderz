76aac25af9817f6296e5ace9f6f65291cf77bff1
Arbab Khan <arbabkhanakr@gmail.com>
2026-04-25 12:41:06 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import base44 from '@base44/vite-plugin'   // ← This was missing
+import { createClient } from '@base44/sdk';
 
-// https://vite.dev/config/
-export default defineConfig({
-  logLevel: 'error', // Suppress warnings, only show errors
-  
-  plugins: [
-    base44({
-      // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
-      // You can set this to false if you've updated all imports to use @base44/sdk
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-      
-      hmrNotifier: true,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: true
-    }),
-    react(),
-  ]
-})
+const base44 = createClient({
+  appId: "69e4ee41ae569fef299bb8db",
+  headers: {
+    "api_key": "56f6c56346834dcf8eace3f7a26e646d"
+  }
+});
